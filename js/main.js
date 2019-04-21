5cb67651ee4a6bf4864a8f47d83ebdfaaf0f6176
SmerhsarP <SmerhsarP@gmail.com>
2019-04-21 16:06:05 +0500

    add popup functions

--- js/main.js
+++ js/main.js
@@ -3,3 +3,24 @@ $(document).ready(function(){
 
   $('.collapsible').collapsible();
 });
+
+$(".callBtn").on("click", function(e) {
+  $(".popupSection").show();
+});
+
+var notClose = 0;
+$(".popupSection").on("click", function() {
+  if (notClose == 1) {
+    notClose = 0;
+  } else {
+    $(".popupSection").hide();
+    notClose = 0;
+  }
+
+});
+$(".popupFormBlock").on("click", function() {
+  notClose = 1;
+});
+$(".closeIcon").on("click", function() {
+  notClose = 1;
+});
